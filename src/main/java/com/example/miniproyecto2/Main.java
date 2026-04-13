5e9043f2d45c3d3cf832a0c5498bd71850151427
Nerie2 <vasqueznerie918@gmail.com>
2026-04-13 13:03:58 -0500

    Model programation

--- src/main/java/com/example/miniproyecto2/Main.java
+++ src/main/java/com/example/miniproyecto2/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         GameStage.getInstance();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/miniproyecto2/model/AbstractSudoku.java
+++ src/main/java/com/example/miniproyecto2/model/AbstractSudoku.java
@@ -1,21 +1,61 @@
 package com.example.miniproyecto2.model;
 
 public abstract class AbstractSudoku implements ISudoku{
+
     int[][] tablero = new int[6][6];
-    
+
     @Override
-    public boolean checkInput() {
-        return false;
+    public boolean checkInput(int x, int y,int number) {
+        if (number<1 || number>6){
+            return false;
+        }
+        int starRow = (x/2)*2;
+        int starCol= (y/3)*3;
+
+        for (int i =0; i <2; i++){
+            for (int j=0; j<3; j++){
+                if (tablero [starRow+i][starCol+j]==number){
+                    return false;
+                }
+            }
+        }
+
+        for (int col=0; col<6; col++){
+            if (tablero[x][col]==number){
+                return false;
+            }
+        }
+
+        for (int row=0; row<6; row++){
+            if (tablero[row][y]==number){
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
-    public String sendInput() {
-        return "";
+    public void sendInput(int x, int y, int number) {
+        if (checkInput(x,y,number)){
+            tablero [x][y]=number;
+        }
     }
 
     @Override
     public String searchInput() {
-        return "";
+        for (int i=0; i <6; i++){
+            for(int j=0; j<6; j++){
+                if (tablero[i][j]==0) {
+                    for (int num=1; num<=6; num++){
+                        if (checkInput(i,j,num)){
+                            return "Pista: en la celda (" + i + "," + j + ") puedes poner el número " + num;
+                        }
+                    }
+                }
+            }
+        }
+        return "No hay pistas disponibles";
     }
 
 }
