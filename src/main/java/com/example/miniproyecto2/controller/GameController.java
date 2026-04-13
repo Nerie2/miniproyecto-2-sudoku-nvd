6fce31ad6a5204de66b97c09cad89a0db48e2b95
lojutus <lojutuselmejor@gmail.com>
2026-04-13 13:30:32 -0500

    Button Settings

--- src/main/java/com/example/miniproyecto2/controller/GameController.java
+++ src/main/java/com/example/miniproyecto2/controller/GameController.java
@@ -1,20 +1,58 @@
 package com.example.miniproyecto2.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 import javax.swing.*;
 
 
 public class GameController {
 
+    public GridPane gridPanel;
     int[] position = new int[2];
     @FXML
     private AnchorPane rootPane;
-    @FXML
-    int[] getButtonData(javafx.scene.control.Button button){
+    public Button getButtonAt(int col, int row) {
+
+        for (Node node : gridPanel.getChildren()) {
+
+            Integer c = GridPane.getColumnIndex(node);
+            Integer r = GridPane.getRowIndex(node);
+
+
+            int colIndex;
+
+            if (c == null) {
+                colIndex = 0;
+            } else {
+                colIndex = c;
+            }
+
+            int rowIndex;
+            if (r == null) {
+                rowIndex = 0;
+            } else {
+                rowIndex = r;
+            }
+
+            if (colIndex == col && rowIndex == row) {
+                return (Button) node;
+            }
+        }
+
+        return null;
+    }
+    public void buttonAction(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        position = getButtonData(button);
+
+    }
+    public int[] getButtonData(javafx.scene.control.Button button){
         try {
             String data = button.getUserData().toString();
             String[] parts = data.split(",");
@@ -71,6 +109,7 @@ public class GameController {
         // Aquí puedes iniciar la lógica del juego
         rootPane.requestFocus();
     }
+
 
 
 }
